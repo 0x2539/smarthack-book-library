48cdd241a81a5e63860912db126941b8afee2020
Stefan Niculae <stefan1niculae@gmail.com>
2016-10-22 18:38:58 +0300

    add author to book

--- SmartLibrary/ClientApp/models.py
+++ SmartLibrary/ClientApp/models.py
@@ -7,6 +7,7 @@ from django.db.models.signals import post_save
 
 class Book(models.Model):
     title = models.CharField(max_length=256, blank=False, null=False)
+    author = models.CharField(max_length=512)
     description = models.CharField(max_length=2048)
     release_date = models.DateField()
     cover_url = models.CharField(max_length=512)
@@ -24,6 +25,12 @@ class Book(models.Model):
         return self.title
 
 
+class Loan(models.Model):
+    user = models.OneToOneField(User)
+    book = models.OneToOneField(Book)
+    date = models.DateField(auto_now=True)
+
+
 class Profile(models.Model):
     # access like: request.user.profile.google_id
     user = models.OneToOneField(User)
@@ -34,18 +41,42 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
+
 post_save.connect(create_user_profile, sender=User)
 
 
 def populate():
-    Book.objects.create(title='Lord of the Rings',
-                        description='Book with dwarfs and elves',
-                        release_date=datetime(day=24, month=1, year=1994),
-                        cover_url='http://cdn.collider.com/wp-content/uploads/2016/07/the-lord-of-the-rings-book-cover.jpg',
-                        genre='Fan').save()
+    # Books
+    lotr = Book.objects.create(
+        title='Lord of the Rings',
+        author='JRR Tolkein',
+        description='Book with dwarfs and elves',
+        release_date=datetime(day=24, month=1, year=1994),
+        cover_url='http://cdn.collider.com/wp-content/uploads/2016/07/the-lord-of-the-rings-book-cover.jpg',
+        genre='Fan')
+    lotr.save()
 
-    Book.objects.create(title='Star Wars',
-                        description='Jedis and Siths',
-                        release_date=datetime(day=31, month=8, year=1994),
-                        cover_url='https://s-media-cache-ak0.pinimg.com/originals/16/86/be/1686bee733af0dbccd952ea34e4955d3.jpg',
-                        genre='SF').save()
+    sw = Book.objects.create(
+        title='Star Wars',
+        author='George Lucas',
+        description='Jedis and Siths',
+        release_date=datetime(day=31, month=8, year=1994),
+        cover_url='https://s-media-cache-ak0.pinimg.com/originals/16/86/be/1686bee733af0dbccd952ea34e4955d3.jpg',
+        genre='SF')
+    sw.save()
+
+    cormen = Book.objects.create(
+        title=''
+    )
+
+    # Users
+    stefan = User.objects.create(username='stefan',
+                                 password='parola123')
+    stefan.save()
+
+    alex = User.objects.create(username='alex',
+                               password='parola123')
+    alex.save()
+
+    # Loans
+    stefan_lotr = Loan.objects.create()
