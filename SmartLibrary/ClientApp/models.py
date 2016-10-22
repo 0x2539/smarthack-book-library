a60e2e0c2a478df83f361eca3aa3be0a4b2484c8
Stefan Niculae <stefan1niculae@gmail.com>
2016-10-22 15:54:54 +0300

    more book info on frontpage

--- SmartLibrary/ClientApp/models.py
+++ SmartLibrary/ClientApp/models.py
@@ -7,8 +7,9 @@ from django.db.models.signals import post_save
 
 class Book(models.Model):
     title = models.CharField(max_length=256, blank=False, null=False)
-    description = models.CharField(max_length=2048, blank=False, null=False)
+    description = models.CharField(max_length=2048)
     release_date = models.DateField()
+    cover_url = models.CharField(max_length=512)
 
     GENRES = [
         ('SF', 'Sci-Fi'),
@@ -37,7 +38,14 @@ post_save.connect(create_user_profile, sender=User)
 
 
 def populate():
-    Book.objects.create(title='Lord of the Rings', description='Book with dwarfs and elves',
-                        release_date=datetime(day=24, month=1, year=1994), genre='Fan').save()
-    Book.objects.create(title='Star Wars', description='Jedis and Siths',
-                        release_date=datetime(day=31, month=8, year=1994), genre='SF').save()
+    Book.objects.create(title='Lord of the Rings',
+                        description='Book with dwarfs and elves',
+                        release_date=datetime(day=24, month=1, year=1994),
+                        cover_url='http://cdn.collider.com/wp-content/uploads/2016/07/the-lord-of-the-rings-book-cover.jpg',
+                        genre='Fan').save()
+
+    Book.objects.create(title='Star Wars',
+                        description='Jedis and Siths',
+                        release_date=datetime(day=31, month=8, year=1994),
+                        cover_url='https://s-media-cache-ak0.pinimg.com/originals/16/86/be/1686bee733af0dbccd952ea34e4955d3.jpg',
+                        genre='SF').save()
