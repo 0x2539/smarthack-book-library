deaf169df05be3fa432c0a11bd842d1b3ccb3a7b
Stefan Niculae <stefan1niculae@gmail.com>
2016-10-22 13:27:20 +0300

    book model

--- SmartLibrary/SmartLibraryBackend/models.py
+++ SmartLibrary/SmartLibraryBackend/models.py
@@ -1,4 +1,17 @@
 from django.db import models
 
-# Create your models here.
-# class Book:
+
+class Book:
+    title = models.CharField(max_length=256, blank=False, null=False)
+    description = models.CharField(max_length=2048, blank=False, null=False)
+    release_date = models.DateField()
+
+    GENRES = [
+        ('SF', 'Sci-Fi'),
+        ('Prog', 'Programming'),
+        ('SelfDev', 'Self Development'),
+        ('R', 'Romance'),
+    ]
+
+    # Example query: Book.objects.filter(genre='SF')
+    genre = models.CharField(choices=GENRES)
